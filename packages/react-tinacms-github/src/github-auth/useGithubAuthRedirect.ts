700f092e5268312972ddf7178edb9c03e72857f6
Logan May <loma@sas.upenn.edu>
2021-01-23 10:33:57 -0500

    feat(react-tinacms-github): log state in callback target page

--- packages/react-tinacms-github/src/github-auth/useGithubAuthRedirect.ts
+++ packages/react-tinacms-github/src/github-auth/useGithubAuthRedirect.ts
@@ -23,6 +23,8 @@ export const useGithubAuthRedirect = () => {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search)
     const code = urlParams.get('code')
+    const state = urlParams.get('state')
+    console.log(`state: ${state}`)
 
     localStorage[GITHUB_AUTH_CODE_KEY] = code
   }, [])
